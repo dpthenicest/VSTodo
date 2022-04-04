a3b86eb663fe38b72b01acd2386b16869b05057e
Daniel Israel <izuisrael27@gmail.com>
2022-04-04 21:53:58 +0100

    first-commit

---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorldPanel";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vstodo" is now active!');
+
+  context.subscriptions.push(vscode.commands.registerCommand("vstodo.helloWorld", () => {
+    HelloWorldPanel.createOrShow(context.extensionUri);
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand("vstodo.refresh", () => {
+    HelloWorldPanel.kill();
+    HelloWorldPanel.createOrShow(context.extensionUri);
+    setTimeout(() => {
+      vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+    }, 500);
+  }))
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstodo.askQuestion", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How are you feeling today?",
+        "good",
+        "bad"
+      );
+
+	  if (answer === "bad") {
+		  vscode.window.showInformationMessage("Sorry about that");
+	  } else {
+		console.log({answer})
+	  }
+    })
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
---
+++ webviews/components/HelloWorld.svelte
@@ -0,0 +1,5 @@
+<script lang='ts'>
+
+</script>
+
+<h1>Hello World, I am Dp😎</h1>
